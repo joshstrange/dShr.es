28db2dbe5021f471831649a9a2c79fcc7f24a64f
Josh Strange <josh@joshstrange.com>
2012-04-03 16:36:30 -0400

    New Ajax

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -98,11 +98,11 @@
 			.getLink .ajax {
 				background-image: url('/img/ajax.gif');
 				width: 16px;
-				height: 11px;
+				height: 16px;
 				z-index: 10;
 				float: right;
-				margin-top: 5px;
-				margin-right: 10px;
+				margin-top: 6px;
+				margin-right: 2px;
 			}
 			.getLink:hover {
 				background-position: 0 0;
