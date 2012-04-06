bf56629e5be38e8569d3d4920eb06368a2249ad9
Josh Strange <josh@joshstrange.com>
2012-04-06 13:04:42 -0400

    Typo

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -381,8 +381,7 @@
 						$('#dbshare_'+pos).html('<input value="'+link+'">');
 						$('#dbshare_'+pos+' input').focus();
 						$('#dbshare_'+pos+' input').select();
-						var twitterLink = '<a href="https://twitter.com/share" class="twitter-share-button" data-url="'+data.url+'" data-text="'+fileName+'" data-via="dShresApp" data-count="none">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+						var twitterLink = '<a href="https://twitter.com/share" class="twitter-share-button" data-url="'+data.url+'" data-text="'+fileName+'" data-via="dShresApp" data-count="none">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 						$('#dbshare_'+pos).html($('#dbshare_'+pos).html()+twitterLink);
 						
 					}
